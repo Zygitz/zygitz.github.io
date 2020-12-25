710f50f1b8b8397376ae01167bb6ba280f1ec481
Zygitz <64617596+Zygitz@users.noreply.github.com>
2020-12-24 19:58:49 -0500

    Family Website

---
+++ script.js
@@ -0,0 +1,59 @@
+var td1row1 = document.getElementById("1td1");
+var td2row1 = document.getElementById("1td2");
+var td3row1 = document.getElementById("1td3");
+var td4row1 = document.getElementById("1td4");
+var td5row1 = document.getElementById("1td5");
+
+var td1row2 = document.getElementById("2td1");
+var td2row2 = document.getElementById("2td2");
+var td3row2 = document.getElementById("2td3");
+var td4row2 = document.getElementById("2td4");
+var td5row2 = document.getElementById("2td5");
+
+var td1row3 = document.getElementById("3td1");
+var td2row3 = document.getElementById("3td2");
+var td3row3 = document.getElementById("3td3");
+var td4row3 = document.getElementById("3td4");
+var td5row3 = document.getElementById("3td5");
+
+var td1row4 = document.getElementById("4td1");
+var td2row4 = document.getElementById("4td2");
+var td3row4 = document.getElementById("4td3");
+var td4row4 = document.getElementById("4td4");
+var td5row4 = document.getElementById("4td5");
+
+var td1row5 = document.getElementById("5td1");
+var td2row5 = document.getElementById("5td2");
+var td3row5 = document.getElementById("5td3");
+var td4row5 = document.getElementById("5td4");
+var td5row5 = document.getElementById("5td5");
+
+td1row1.addEventListener("click", function(event) {if (td1row1.bgColor == "red") {td1row1.bgColor = "white"} else {td1row1.bgColor = "red"}});
+td2row1.addEventListener("click", function(event) {if (td2row1.bgColor == "red") {td2row1.bgColor = "white"} else {td2row1.bgColor = "red"}});
+td3row1.addEventListener("click", function(event) {if (td3row1.bgColor == "red") {td3row1.bgColor = "white"} else {td3row1.bgColor = "red"}});
+td4row1.addEventListener("click", function(event) {if (td4row1.bgColor == "red") {td4row1.bgColor = "white"} else {td4row1.bgColor = "red"}});
+td5row1.addEventListener("click", function(event) {if (td5row1.bgColor == "red") {td5row1.bgColor = "white"} else {td5row1.bgColor = "red"}});
+
+td1row2.addEventListener("click", function(event) {if (td1row2.bgColor == "red") {td1row2.bgColor = "white"} else {td1row2.bgColor = "red"}});
+td2row2.addEventListener("click", function(event) {if (td2row2.bgColor == "red") {td2row2.bgColor = "white"} else {td2row2.bgColor = "red"}});
+td3row2.addEventListener("click", function(event) {if (td3row2.bgColor == "red") {td3row2.bgColor = "white"} else {td3row2.bgColor = "red"}});
+td4row2.addEventListener("click", function(event) {if (td4row2.bgColor == "red") {td4row2.bgColor = "white"} else {td4row2.bgColor = "red"}});
+td5row2.addEventListener("click", function(event) {if (td5row2.bgColor == "red") {td5row2.bgColor = "white"} else {td5row2.bgColor = "red"}});
+
+td1row3.addEventListener("click", function(event) {if (td1row3.bgColor == "red") {td1row3.bgColor = "white"} else {td1row3.bgColor = "red"}});
+td2row3.addEventListener("click", function(event) {if (td2row3.bgColor == "red") {td2row3.bgColor = "white"} else {td2row3.bgColor = "red"}});
+td3row3.addEventListener("click", function(event) {if (td3row3.bgColor == "red") {td3row3.bgColor = "white"} else {td3row3.bgColor = "red"}});
+td4row3.addEventListener("click", function(event) {if (td4row3.bgColor == "red") {td4row3.bgColor = "white"} else {td4row3.bgColor = "red"}});
+td5row3.addEventListener("click", function(event) {if (td5row3.bgColor == "red") {td5row3.bgColor = "white"} else {td5row3.bgColor = "red"}});
+
+td1row4.addEventListener("click", function(event) {if (td1row4.bgColor == "red") {td1row4.bgColor = "white"} else {td1row4.bgColor = "red"}});
+td2row4.addEventListener("click", function(event) {if (td2row4.bgColor == "red") {td2row4.bgColor = "white"} else {td2row4.bgColor = "red"}});
+td3row4.addEventListener("click", function(event) {if (td3row4.bgColor == "red") {td3row4.bgColor = "white"} else {td3row4.bgColor = "red"}});
+td4row4.addEventListener("click", function(event) {if (td4row4.bgColor == "red") {td4row4.bgColor = "white"} else {td4row4.bgColor = "red"}});
+td5row4.addEventListener("click", function(event) {if (td5row4.bgColor == "red") {td5row4.bgColor = "white"} else {td5row4.bgColor = "red"}});
+
+td1row5.addEventListener("click", function(event) {if (td1row5.bgColor == "red") {td1row5.bgColor = "white"} else {td1row5.bgColor = "red"}});
+td2row5.addEventListener("click", function(event) {if (td2row5.bgColor == "red") {td2row5.bgColor = "white"} else {td2row5.bgColor = "red"}});
+td3row5.addEventListener("click", function(event) {if (td3row5.bgColor == "red") {td3row5.bgColor = "white"} else {td3row5.bgColor = "red"}});
+td4row5.addEventListener("click", function(event) {if (td4row5.bgColor == "red") {td4row5.bgColor = "white"} else {td4row5.bgColor = "red"}});
+td5row5.addEventListener("click", function(event) {if (td5row5.bgColor == "red") {td5row5.bgColor = "white"} else {td5row5.bgColor = "red"}});
